59e12b4668f06e1100ce7353438f2b33d7395166
gcww <gcww_qzq@126.com>
2018-04-08 20:57:42 +0800

    branch test1
    branch test1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { HeaderLoginService } from './service/header-login.service';
     AuthorIntroduceComponent,
   ],
   imports: [
-    HealthyModule,//HealthyModule不知到这么引入该模块的其他内容是不是也加载进来了
+    HealthyModule,//HealthyModule不知到这么引入该模块的其他内容是不是也加载进来了，再随便加一句
     BrowserModule,
     FormsModule,
     HttpClientModule,
